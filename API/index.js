4a38314f316cf42f0c1327026d0f8b24d52b284d
Abid Ali <49807758+Abidali08@users.noreply.github.com>
2026-06-02 10:47:59 +0000

    GET API and FETCh ON CLIENT

--- API/index.js
+++ API/index.js
@@ -4,29 +4,79 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 const pool = new Pool({
-    connectionString : process.env.DATABASE_URL,
-    ssl:{
-        rejectUnauthorized : false
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+app.get('/', (req, res) => {
+    try {
+        res.json('WELCOME')
+    } catch (err) {
+        res.status(500).json({ error: err.message })
     }
 });
 
 
-app.get('/', async(req,res)=>{
-    try{
+app.get('/employees', async (req, res) => {
+    try {
         const result = await pool.query('select * from employees');
         res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
 
-    }catch(err){
-        res.status(500).json({error:err.message});
+app.get('/regions', async (req, res) => {
+    try {
+        const result = await pool.query('select * from regions');
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.get('/countries', async (req, res) => {
+    try {
+        const result = await pool.query('select * from countries');
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.get('/hrdetails', async (req, res) => {
+    try {
+        const result = await pool.query('select * from hrdetails');
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.get('/dptcal', async (req, res) => {
+    try {
+        const result = await pool.query(`
+            select d.department_name,count(e.employee_id) as TotalEmp
+            from employees e right join departments d
+            on e.department_id = d.department_id
+            group by department_name
+            order by department_name;
+            `);
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
 });
 
 
-const PORT =  3000;
-app.listen(PORT,()=>{
-    console.log(`SERVER RUNNING ON PORT ${PORT}`)
+
+
+app.listen(3000, () => {
+    console.log('RUNNING ON PORT 3000');
 });
